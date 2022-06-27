1358cc6f48619edec21295da09de718057092cce
Cristopher Molina <102634932+CristopherMolina@users.noreply.github.com>
2022-06-26 21:31:16 -0500

    Add files via upload

---
+++ app/firebaseConfig.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  "apiKey": "AIzaSyAkQ0EQPnw5nmAzQ8zMUzM7BPEbS_4xwfA",
+  "authDomain": "cmrepositoriog15.firebaseapp.com",
+  "projectId": "cmrepositoriog15",
+  "storageBucket": "cmrepositoriog15.appspot.com",
+  "messagingSenderId": "972886493178",
+  "appId": "1:972886493178:web:74900b688ac348c7638643",
+  "measurementId": "G-VJG24K3CQ3",
+  "databaseURL": ""
+}
